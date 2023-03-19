f89f8d812f3af8c5268f80d479c97fabec4db99e
noponet <128297406+noponet@users.noreply.github.com>
2023-03-19 06:07:29 -0700

    Add files via upload

---
+++ LockServer/LockServer/Connection.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockServer
+{
+
+    class Connection
+    {
+
+        private LockServer server;
+        private Socket socket;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private bool running;
+
+		public Connection(LockServer server, Socket socket)
+		{
+			this.server = server;
+			this.socket = socket;
+
+			try
+			{
+				NetworkStream stream = new NetworkStream(socket);
+				this.reader = new StreamReader(stream);
+				this.writer = new StreamWriter(stream);
+
+			}
+			catch (IOException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+		public void run()
+		{
+			// TODO Auto-generated method stub
+			running = true;
+			while (running)
+			{
+
+				try
+				{
+					String request = reader.ReadLine();
+					Handle(request);
+				}
+				catch (IOException e)
+				{
+
+                    Console.WriteLine("Connection closed");
+					this.Close();
+				}
+			}
+		}
+        private void Handle(string request)
+        {
+            Console.WriteLine(request);
+            if (request != null)
+            {
+                string[] parameters = request.Split('<');
+
+                switch (parameters[0])
+                {
+
+                    case "CHECK":
+                        if (server.GetConfig().ContainsKey(parameters[1]))
+                        {
+                            Console.WriteLine(parameters[1]);
+                            int timeLeft = server.GetConfig()[parameters[1]] - server.CurrentTimeMinutes();
+                            if (timeLeft < 0)
+                                timeLeft = 0;
+                            Send("RESPONSE", CheckString(timeLeft));
+                        }
+                        else
+                        {
+                            Send("RESPONSE", "Invalid file name");
+                        }
+                        break;
+
+                    case "LIST":
+                        string list = "";
+                        foreach (string file2 in Directory.GetFiles(LockServer.FILES_DIR))
+                        {
+                            list += Path.GetFileName(file2) + ">";
+                        }
+                        Send("LIST", list);
+                        break;
+
+                    case "SET":
+                        string file = parameters[1];
+
+                        int time = int.Parse(parameters[2]);
+                        string response = server.SetTime(file, time + server.CurrentTimeMinutes());
+                        Send("RESPONSE", response);
+                        break;
+
+                    case "PUSH":
+                        string inputFile = parameters[1];
+                        try
+                        {
+                            File.Copy(inputFile, LockServer.FILES_DIR + Path.GetFileName(inputFile), true);
+                            string list2 = "";
+                            foreach (string file2 in Directory.GetFiles(LockServer.FILES_DIR))
+                            {
+                                list2 += Path.GetFileName(file2) + ">";
+                            }
+                            Send("LIST", list2);
+                        }
+                        catch (IOException e)
+                        {
+                            Send("RESPONSE", "error copying file");
+                            Console.WriteLine(e);
+                        }
+                        break;
+
+                    case "CLONE":
+                        string fileToCopy = LockServer.FILES_DIR + parameters[1];
+
+                        if ((!server.GetConfig().ContainsKey(parameters[1])) || (server.CurrentTimeMinutes() - server.GetConfig()[parameters[1]]) >= 0)
+                        {
+                            try
+                            {
+                                if (File.Exists(fileToCopy) && Directory.Exists(parameters[2]))
+                                {
+
+                                    File.Copy(fileToCopy, parameters[2] + "/" + parameters[1], true);
+                                    Send("RESPONSE", "ok");
+                                }
+                                else
+                                {
+                                    Send("RESPONSE", "error copying file");
+                                }
+                            }
+                            catch (IOException e)
+                            {
+                                Send("RESPONSE", "error copying file");
+                                Console.WriteLine(e);
+                            }
+                        }
+                        else
+                        {
+                            Send("RESPONSE", "I can't let you do that, " + CheckString(server.GetConfig()[parameters[1]] - server.CurrentTimeMinutes()) + " left.");
+                        }
+                        break;
+                }
+            }
+        }
+        private string CheckString(int remainder)
+        {
+            int hoursLeft = remainder / 60;
+            int daysLeft = hoursLeft / 24;
+
+            int remainder_hours = hoursLeft - daysLeft * 24;
+            int remainder_minutes = remainder - (daysLeft * 24 * 60 + remainder_hours * 60);
+            return (daysLeft + " days " + remainder_hours + " hours " + remainder_minutes + " minutes");
+        }
+
+        private void Send(params string[] parameters)
+        {
+            string response = string.Join("<", parameters);
+            writer.WriteLine(response);
+            writer.Flush();
+        }
+
+        public void Close()
+        {
+            try
+            {
+                this.socket.Close();
+                this.reader.Close();
+                this.writer.Close();
+                running = false;
+            }
+            catch (IOException e)
+            {
+                // TODO: Handle the IOException
+                Console.WriteLine(e);
+            }
+        }
+
+
+    }
+}
---
+++ LockServer/LocksServer.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockServer
+{
+    class LockServer
+    {
+        private Socket serverSocket;
+        private bool running;
+        public static String FILES_DIR = "C:\\Program Files (x86)\\noponet\\files\\";
+	    public static String DIR = "C:\\Program Files (x86)\\noponet";
+	    public static String TIME_SERVER = "https://currentmillis.com/time/minutes-since-unix-epoch.php";
+        private Dictionary<string, int> config = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+            LockServer server = new LockServer();
+            new Thread(server.run).Start();
+
+        }
+        public LockServer()
+        {
+            LoadConfig();
+        }
+        public void run()
+        {
+            running = true;
+            // Create an endpoint to listen on
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 6778);
+
+            // Create a TCP socket to listen for incoming connections
+            this.serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the endpoint
+            serverSocket.Bind(endpoint);
+
+            // Start listening for incoming connections
+            serverSocket.Listen(5);
+
+            Console.WriteLine("Server is running and listening for incoming connections...");
+
+            foreach (string file2 in Directory.GetFiles(LockServer.FILES_DIR))
+            {
+                if (!config.ContainsKey(file2))
+                {
+                    WriteConfig(file2, 0);
+                }
+            }
+                while (running)
+            {
+                // Accept an incoming connection
+                Socket clientSocket = serverSocket.Accept();
+                new Thread(new Connection(this, clientSocket).run).Start();
+            }
+
+        }
+
+        private void LoadConfig()
+        {
+            string configFile = Path.Combine(DIR, "config");
+
+            if (File.Exists(configFile))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(configFile))
+                    {
+                        string line;
+                        string[] parameters;
+
+                        while (!reader.EndOfStream)
+                        {
+                            line = reader.ReadLine();
+                            parameters = line.Split('>');
+                            config.Add(parameters[0], int.Parse(parameters[1]));
+                        }
+                        reader.Close();
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    // TODO: Handle the FileNotFoundException
+                    Console.WriteLine(e);
+                }
+                catch (IOException e)
+                {
+                    // TODO: Handle the IOException
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                try
+                {
+                    File.Create(configFile);
+                }
+                catch (IOException e)
+                {
+                    // TODO: Handle the IOException
+                    Console.WriteLine(e);
+                }
+            }
+        }
+        private void WriteConfig(string file, int time)
+        {
+            string configFile = Path.Combine(DIR, "config");
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(configFile);
+                
+                    config[file] = time;
+
+                    foreach (string f in config.Keys)
+                    {
+                        writer.WriteLine(f + ">" + config[f].ToString());
+                    }
+
+                    config.Clear();
+                    
+                    writer.Close();
+                LoadConfig();
+            }
+            
+            catch (IOException e)
+            {
+                // TODO: Handle the IOException
+                Console.WriteLine(e);
+            }
+        }
+
+        public string SetTime(string fileName, int time)
+        {
+            lock (this)
+            {
+                if (config.ContainsKey(fileName))
+                {
+                    if (CurrentTimeMinutes() >= config[fileName])
+                    {
+                        WriteConfig(fileName, time);
+                        return "ok";
+                    }
+                    else
+                    {
+                        return "I can't let you do that, time difference is " + (config[fileName] - CurrentTimeMinutes()).ToString() + " minutes.";
+                    }
+                }
+                else
+                {
+                    WriteConfig(fileName, time);
+                    return "ok";
+                }
+            }
+        }
+
+        public int CurrentTimeMinutes()
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create(TIME_SERVER);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                int time = int.Parse(reader.ReadLine());
+                reader.Close();
+                response.Close();
+                return time;
+            }
+            catch (WebException e)
+            {
+                // TODO: Handle the WebException
+                Console.WriteLine(e);
+            }
+            return 0;
+        }
+
+        public Dictionary<string, int> GetConfig()
+        {
+            return config;
+        }
+
+
+    }
+}
